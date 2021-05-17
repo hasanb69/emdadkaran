bb7213b579984dd0db828b9f263152a6c0b073d9
unknown <hasanbaratiasp@gmail.com>
2021-05-17 17:53:14 +0430

    read data from firebase

--- src/component/category/category.jsx
+++ src/component/category/category.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import SubMainCategory from '../category/submaincategory'
 import {addCollectionAndDocuments} from '../../firebase/firebase'
+import {firestore,convertCollectionsSnapshotToMap } from '../../firebase/firebase'
 import {connect} from 'react-redux'
-const Category=({data})=>{
+import {updatadata} from '../../redux/shop/shop.action'
+const Category=({data,updatadata})=>{
 
     const collection=Object.keys(data).map(key => data[key])
+useEffect(()=>{
+    const collectionRef = firestore.collection('collections');
+    collectionRef.get().then((snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        updatadata(collectionsMap);
+      //  console.log(collectionsMap)
+      },[updatadata]);
+   
+})
+    
 
-    addCollectionAndDocuments('collections',collection.map(({title,items})=>({title,items})))
+   // addCollectionAndDocuments('collections',collection.map(({title,items})=>({title,items})))
     return(
     <div className="home-category">
         {collection.map(({id,...other})=>(
@@ -16,7 +28,10 @@ const Category=({data})=>{
 
     )
 }
+const mapDispatchToProps=dispatch=>({
+    updatadata:collectionMap=>dispatch(updatadata(collectionMap))
+})
 const mapStateToProps=state=>({
     data:state.shop.shopdata
   })
-export default connect(mapStateToProps)(Category)
+export default connect(mapStateToProps,mapDispatchToProps)(Category)
